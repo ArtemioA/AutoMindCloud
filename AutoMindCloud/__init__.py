0dc5a042440d523634dfea78b4d7c22690cbe2ed
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-06-30 22:52:01 -0400

    Update __init__.py

--- AutoMindCloud/__init__.py
+++ AutoMindCloud/__init__.py
@@ -1,6 +1,5 @@
 from IPython.display import display, Image
 
-display(Image(url='https://i.gyazo.com/5d4120be41b4cc570344957111f844de.png'))
-
+display(Image(filename="AutoMindCloud/AutoMindCloud/AutoMindCloud.png"))
 
 
