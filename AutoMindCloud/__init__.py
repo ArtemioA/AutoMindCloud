7d9a8f10e5dc5f3f10906fbd50680b7fa070323b
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-07-01 19:49:21 -0400

    Update __init__.py

--- AutoMindCloud/__init__.py
+++ AutoMindCloud/__init__.py
@@ -1,13 +1,16 @@
+from IPython.display import display, Image
 import os
-from IPython.display import Image, display
 
-image_name = "AutoMindCloud.png"
-image_path = None
+def show_image():
+    # Get the directory where __init__.py is located
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    image_path = os.path.join(dir_path, 'AutoMindCloud.png')
+    
+    # Check if the image exists
+    if os.path.exists(image_path):
+        display(Image(filename=image_path))
+    else:
+        print(f"Image not found at: {image_path}")
 
-for root, dirs, files in os.walk('.'):
-    if image_name in files:
-        image_path = os.path.join(root, image_name)
-        break
-
-if image_path:
-    display(Image(filename=image_path))
+# Optionally, automatically display the image when the module is imported
+show_image()
