e3571f83067b0407e3ac6b11dfb5281a04a2ec3d
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-07-01 00:35:50 -0400

    Update Latemix2.py

--- AutoMindCloud/Latemix2.py
+++ AutoMindCloud/Latemix2.py
@@ -3114,21 +3114,18 @@ def latex(expr, **settings):
 
 __all__ = ["RenderLatex"]
 
-def RenderLatex(info, **settings):
+def latemix(info, **settings):
+    display(info)
   
-  #from AutoMindCloud.LatexRenderScript import search
-  
-  if len(info) == 2: 
-  #if isinstance(info, list):
-    global DatosList
+    if len(info) == 2: 
+        global DatosList
 
-    expr = info[0]
+        expr = info[0]
 
-    DatosList = info[1]
+        DatosList = info[1]
 
-  else:
-    
-    expr = info
+    if len(info) == 1:
+        expr = info
     """Prints LaTeX representation of the given expression. Takes the same
     settings as ``latex()``."""
 
