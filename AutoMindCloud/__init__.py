ca11196cedf0d826d0638bcc34d589536755a688
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-07-01 19:45:58 -0400

    Update __init__.py

--- AutoMindCloud/__init__.py
+++ AutoMindCloud/__init__.py
@@ -1,3 +1,13 @@
-#from IPython.display import display, Image
+import os
+from IPython.display import Image, display
 
-#display(Image(filename="AutoMindCloud/AutoMindCloud.png"))
+image_name = "AutoMindCloud.png"
+image_path = None
+
+for root, dirs, files in os.walk('.'):
+    if image_name in files:
+        image_path = os.path.join(root, image_name)
+        break
+
+if image_path:
+    display(Image(filename=image_path))
