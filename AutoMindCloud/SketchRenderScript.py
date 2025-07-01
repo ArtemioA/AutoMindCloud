18cef7248245f60c6c83a7b89a92afcba520f38a
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-07-01 01:04:17 -0400

    Update SketchRenderScript.py

--- AutoMindCloud/SketchRenderScript.py
+++ AutoMindCloud/SketchRenderScript.py
@@ -6,7 +6,7 @@ from IPython.display import display, SVG
 #result = cq.importers.importStep('Sketch.step')
 # Function to rotate and display the object
 
-def show_view(rotate_axis, rotate_angle, title=""):
+def show_view(result,rotate_axis, rotate_angle, title=""):
     # Rotate the object
     rotated = result.rotate((0,0,0), rotate_axis, rotate_angle)
     # Export as SVG
@@ -14,14 +14,15 @@ def show_view(rotate_axis, rotate_angle, title=""):
     display(SVG(svg_str))
     print(title)
 
-def SketchRender(Sketch):
+def SketchRender(Sketch_Name):
+  result = cq.importers.importStep(Sketch_Name)
   # Standard views using rotation
-  show_view((1,0,0), 0, "Front View")
-  show_view((1,0,0), 90, "Top View")
-  show_view((0,1,0), 90, "Right Side View")
+  show_view(result,(1,0,0), 0, "Front View")
+  show_view(result,(1,0,0), 90, "Top View")
+  show_view(result,(0,1,0), 90, "Right Side View")
 
   # Isometric view (rotate 45° around X then 30° around Z)
-  rotated = Sketch.rotate((0,0,0), (1,0,0), 45).rotate((0,0,0), (0,0,1), 30)
-  svg_str = exporters.getSVG(rotated.val())
-  display(SVG(svg_str))
-  print("Isometric View")
+  #rotated = result.rotate((0,0,0), (1,0,0), 45).rotate((0,0,0), (0,0,1), 30)
+  #svg_str = exporters.getSVG(rotated.val())
+  #display(SVG(svg_str))
+  #print("Isometric View")
