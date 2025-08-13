91a2f1b212c7c698631bd796b562ba207867c74b
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-08-13 15:35:20 -0400

    Update Step_Render_Script.py

--- AutoMindCloud/Step_Render_Script.py
+++ AutoMindCloud/Step_Render_Script.py
@@ -89,7 +89,7 @@ def Step_Render(Step_Name):
             const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
             camera.position.set(0, 0, 3);
 
-            const renderer = new THREE.WebGLRenderer({ antialias: true });
+            const renderer = new THREE.WebGLRenderer({{ antialias: true }});
             renderer.setSize(window.innerWidth, window.innerHeight);
             document.body.appendChild(renderer.domElement);
 
